32eb5dbe5768325cd4aebffaa6dfd27c9f35f347
Your Name <you@example.com>
2022-03-30 10:25:20 +0500

    Initial commit new1

--- ahb_uvm_env.sv
+++ ahb_uvm_env.sv
@@ -5,6 +5,7 @@ class ahb_env extends uvm_env;
     //ENV has agent as its sub component
     ahb_agent      agent;
     ahb_scoreboard scoreboard;
+    int num_packets;
 //    ahb_subscriber subscriber;
    
     //virtual interface for AHB interface
